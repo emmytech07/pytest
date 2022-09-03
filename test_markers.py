a4317e0a089e40042d95decca15ace8ebea37d53
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-03 05:14:31 +0100

    Adding fails and script version to the code

--- test_markers.py
+++ test_markers.py
@@ -1,5 +1,5 @@
-from re import L
 import pytest
+import sys
 
 @pytest.mark.smoke
 def test_login():
@@ -12,3 +12,16 @@ def test_product():
 @pytest.mark.smoke
 def test_logut():
     print("Logging out of the system")
+
+@pytest.mark.skip
+def test_skipped():
+    print("skipping this test function")
+
+@pytest.mark.skipif(sys.version_info < (3,11), reason="Python version not supported")
+def test_skippedif():
+    print("Version is too slow for the process ")
+
+@pytest.mark.xfail
+def test_xfail():
+    assert True
+    print("skipping this test function")
