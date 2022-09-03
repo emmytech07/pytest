047a11ef44601bc697b1abde3584e3f63d830cae
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-03 06:02:34 +0100

    Automating facebook page using webchrome driver

--- test_fixtures.py
+++ test_fixtures.py
@@ -10,21 +10,30 @@ Postcon
 
 """
 import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver= None
 
 @pytest.fixture
 def setup():
+    global driver
     print("Start Browser")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     yield
+    driver.quit()
     print("Close Browser")
 
 def test_1(setup):
+    driver.get("https://www.facebook.com")
     print("Test 1 executed")
     
 
 def test_2(setup):
+    driver.get("https://www.facebook.com")
     print("Test 2 executed")
     
 
 def test_3(setup):
+    driver.get("https://www.facebook.com")
     print("Test 3 executed")
     
